fcf5eb9359e433fde7c501a2ad416a5b66c723d1
TroyAWilson <38123313+TroyAWilson@users.noreply.github.com>
2018-05-11 11:44:16 -0700

    Fixed scripts typo/moved svg

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+//
+// //On play the first span gets orange text
+// if(video.currentTime == 0)
+//    span1.addClass = "orangeText"
+//
+// //as long as the video is playing
+// while(video.active){
+// (time) =>{ //takes an argument of time
+//   if(video.currentTime == time) //if the time is equal to the current time of the video
+//   span1.removeClass()//take away the orange text from the first span
+//   span1.sibling.addClass == "orangeText"//gives the orange text to the next span
+// }
+// }
