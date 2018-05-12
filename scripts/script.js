c79f9e678cc5283131d8a07ef04fc2941bb14996
TroyAWilson <38123313+TroyAWilson@users.noreply.github.com>
2018-05-12 12:17:47 -0700

    Figured out the Text color change

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,25 @@
-//
-// //On play the first span gets orange text
-// if(video.currentTime == 0)
-//    span1.addClass = "orangeText"
-//
-// //as long as the video is playing
-// while(video.active){
-// (time) =>{ //takes an argument of time
-//   if(video.currentTime == time) //if the time is equal to the current time of the video
-//   span1.removeClass()//take away the orange text from the first span
-//   span1.sibling.addClass == "orangeText"//gives the orange text to the next span
-// }
-// }
+//get video
 videoPlayer = document.getElementById("myVideo");
+//get array of spans
+spanArray = document.getElementsByClassName('spanText');
+//everytime the video time updates check for changes
+videoPlayer.ontimeupdate = () => {
+  //loops though array
+  for(i = 0; i < spanArray.length; i++){
+//if the current time is greater than or equal to the start time
+       if (videoPlayer.currentTime >= spanArray[i].dataset.start)
+        {   //add the class
+          spanArray[i].classList.add('currentText');
+        }
+//if the current time is greater than the end time
+      if (videoPlayer.currentTime >= spanArray[i].dataset.end)
+        { //remove the class
+          spanArray[i].classList.remove('currentText');
+        }
+     };
+  }
 
-function videoStartCheck(){
-    $('.spanStart').addClass('currentText');
-}
 
-$(videoPlayer).on('click', ()=>{
-  videoStartCheck();
-})
-
-
-// $('.video').on('click', function() {
-//     $('span1').addClass('currentText');
-// });
+// videoPlayer.ontimeupdate = () => {
+//   console.log(videoPlayer.currentTime);
+// }
